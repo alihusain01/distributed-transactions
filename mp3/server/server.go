1af756d36131a0a9a8bb6c0a7e9d88c7551403f8
satejrs2 <satejrs2@sp24-cs425-4201.cs.illinois.edu>
2024-04-24 17:59:52 -0500

    Added server connection code

--- mp3/server/server.go
+++ mp3/server/server.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 	"os"
+	"strings"
 )
+
+type Server struct {
+	Name string
+	Host string
+	Port string
+}
+
+func readConfigFile(filename string) ([]Server, error) {
+	var servers []Server
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+		if len(parts) >= 3 {
+			server := Server{
+				Name: parts[0],
+				Host: parts[1],
+				Port: parts[2],
+			}
+			servers = append(servers, server)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return servers, nil
+}
+
+func establishConnections(servers []Server) {
+	for _, server := range servers {
+		conn, err := net.Dial("tcp", server.Host+":"+server.Port)
+		if err != nil {
+			fmt.Printf("Error connecting to %s at %s on port %s: %s\n", server.Name, server.Host, server.Port, err)
+			continue
+		}
+		defer conn.Close()
+		fmt.Printf("Connected to %s at %s on port %s\n", server.Name, server.Host, server.Port)
+		// You can use 'conn' for further communication with the server if needed
+	}
+}
 
 func main() {
 	args := os.Args[1:]
@@ -21,5 +70,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Branch: %s\nConfig file: %s\n", branch, configFile)
+	servers, err := readConfigFile(configFile)
+	if err != nil {
+		fmt.Println("Error reading config file:", err)
+		return
+	}
+
+	establishConnections(servers)
+
+	fmt.Printf("Branch %s has been successfully connected to all servers.\n", branch)
 }
